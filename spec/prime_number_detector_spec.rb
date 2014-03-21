abef53880165594fe6df2c49df6c4570c6d6841a
Bebe Peng <bebepeng@gmail.com>
2014-03-21 09:20:57 -0600

    refactored spec to eliminate repitition of instantiantion

--- spec/prime_number_detector_spec.rb
+++ spec/prime_number_detector_spec.rb
@@ -1,12 +1,14 @@
 require 'prime_number_detector'
 
 describe PrimeNumberDetector do
+  before do
+    @detector = PrimeNumberDetector.new
+  end
+
   it "recognizes if something is a prime number" do
-    detector = PrimeNumberDetector.new
-    expect(detector.prime_number?(3)).to eq true
+    expect(@detector.prime_number?(3)).to eq true
   end
   it "recogizes if something is not a prime number" do
-    detector = PrimeNumberDetector.new
-    expect(detector.prime_number?(10)).to eq false
+    expect(@detector.prime_number?(10)).to eq false
   end
 end
